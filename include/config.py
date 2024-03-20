f74daf4693c1642a63aefb9ac11f66955df77b2e
Hitomi Shibata <78398933+hitomishibata@users.noreply.github.com>
2024-03-20 13:11:29 +0100

    Update config.py

--- include/config.py
+++ include/config.py
@@ -1,12 +1,12 @@
 # Databricks notebook source
 raw_folder_path = 'abfss://raw@dludemyes.dfs.core.windows.net'
 processed_folder_path = 'abfss://processed@dludemyes.dfs.core.windows.net'
-presentation_folder_path = 'abfss://presentation@dludemyes.dfs.core.windows.net'
+
 
 
 # COMMAND ----------
 
 spark.conf.set(
     "fs.azure.account.key.dludemyes.dfs.core.windows.net",
-    "Tt1gweEejiPIA5ODGth2hES63ZifDSVd5tQPY8mxoV1O8bXaE8B8F4zcFG7tec3fs4PqM7ImpAEh+ASt0tonkQ=="
+    "youraccountkey"
 )
